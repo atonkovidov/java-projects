76a7fe0069ef77ee3ea3d28f83e587238285ab24
atonkovidov <59673687+atonkovidov@users.noreply.github.com>
2020-01-11 19:19:05 -0800

    Week 13

---
+++ Week 13/Assignment2.java
@@ -0,0 +1,47 @@
+// Recursive method to display all permutations of a string
+
+import java.util.Scanner;
+
+public class Assingment2 {
+
+	public static void main(String[] args) {
+		// Entering the initial string
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		String s = input.nextLine();
+		input.close();
+		
+		// Displaying all permutations of String s
+		System.out.println("\nThe permutations of the string " + s + " are");
+		displayPermutation(s);
+	}
+	
+	public static void displayPermutation(String s) {
+		displayPermutation("", s);
+	}
+	
+	public static void displayPermutation(String s1, String s2) {
+		if (s2.length() == 0)
+			System.out.println(s1);
+		for (int i = 0; i < s2.length(); i++) {
+			String s3 = s1 + s2.charAt(i);
+			String s4 = s2.substring(0, i) + s2.substring(i + 1);
+			displayPermutation(s3, s4);
+		}
+	}
+}
+
+
+/* Output
+
+Enter a string: abc
+
+The permutations of the string abc are
+abc
+acb
+bac
+bca
+cab
+cba
+
+*/
---
+++ Week 13/Assignment3.java
@@ -0,0 +1,72 @@
+// Program to play the Teddy Bear game with the goal of 42
+
+import java.util.*;
+
+public class Assignment3 {
+// solution by Takyiu Liu, West Valley College, summer 2019
+	public static void main(String[] args) {
+		Stack<String> stack = new Stack<>();
+		for (int i = 50; i <= 500; i++) {
+			System.out.println("\nStarting number:" + i);
+			if (bears(stack, i))
+				while (!stack.isEmpty())
+					System.out.println(stack.pop());
+			else
+				System.out.println("No solution");
+		}
+	}
+
+	static boolean bears(Stack<String> stack, int n) {
+		int product;
+		if (n == 42)
+			return true;
+		else if (n < 42)
+			// your work
+			return false;
+		else if ((n % 2 == 0) && bears(stack, n / 2)) {
+			// your work
+			stack.push("n=" + n + ", return " + n / 2 + ". Now n=" + n / 2 + ".");
+			return true;
+		}
+		else if ((n % 5 == 0) && bears(stack, n - 42)) {
+			// your work
+			stack.push("n=" + n + ", return " + 42 + ". Now n=" + (n - 42) + ".");
+			return true;
+		}
+		else {
+			product = ((n % 100) / 10) * (n % 10);
+			// your work
+			if (product == 0)
+				return false;
+			else if (((n % 3 == 0) && bears(stack, n - product)) || ((n % 4 == 0) && bears(stack, n - product))) {
+				stack.push("n=" + n + ", return " + product + ". Now n=" + (n - product) + ".");
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+
+/* Output (last 3 numbers: 498, 499, 500)
+
+Starting number:498
+n=498, return 249. Now n=249.
+n=249, return 36. Now n=213.
+n=213, return 3. Now n=210.
+n=210, return 42. Now n=168.
+n=168, return 84. Now n=84.
+n=84, return 42. Now n=42.
+
+Starting number:499
+No solution
+
+Starting number:500
+n=500, return 250. Now n=250.
+n=250, return 42. Now n=208.
+n=208, return 104. Now n=104.
+n=104, return 52. Now n=52.
+n=52, return 10. Now n=42.
+
+
+*/
