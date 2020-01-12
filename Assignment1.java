8c7243594369e17c5b7b1922c1b90750d540fed6
atonkovidov <59673687+atonkovidov@users.noreply.github.com>
2020-01-11 19:16:14 -0800

    Week 12

---
+++ Assignment1.java
@@ -0,0 +1,25 @@
+// Adding numerous fractions together using the given Rational class
+
+public class Assignment1 {
+	public static void main(String[] args) {
+		// Initializing values in preparation for loop
+		final int LAST_VALUE = 19;
+		Rational fraction = new Rational();
+		
+		// Main loop for adding fractions
+		for (int i = 1; i <= LAST_VALUE; i++) {
+			Rational temporaryFraction = new Rational(i, i + 1);
+			fraction = fraction.add(temporaryFraction);
+		}
+		
+		// Printing out the final rational number in fraction form
+		System.out.print(fraction.toString());
+	}
+}
+
+
+/* Output (took a while to compute)
+
+254554945/15519504
+
+*/
